cc449040db79368dd4635340312598f103800397
ismail karakaş <43730835+isml@users.noreply.github.com>
2021-01-23 20:14:05 +0300

    Update finalODEV.m

--- BMOK_ODEV_KODLAR/finalODEV.m
+++ BMOK_ODEV_KODLAR/finalODEV.m
@@ -1,6 +1,6 @@
 clear all;
-FG =imread("C:/Users/ismail/Desktop/d/deneme.jpg");
-BG =imread("C:/Users/ismail/Desktop/d/denemebg.jpg");
+FG =imread("C:/Users/ismail/Desktop/d/1.jpg");
+BG =imread("C:/Users/ismail/Desktop/d/bg1.jpg");
 #ON YUZ FOTOGRAFI VE ARKA PLAN FOTOGRAFINI ?MREAD ?LE OKUNDU[1-2]
 FGhsv = rgb2hsv(FG);
 #ON YUZ FOTOGRAFI HSV RENK UZAYINA DÖNÜ?TÜRÜLDÜ[5]
@@ -12,11 +12,11 @@ S=uint8(255*FGhsv(:,:,1));
 V=uint8(255*FGhsv(:,:,1));
 #HSV ON YUZ FOTOGRAFININ HUE,SATURATION VE VALUE KATMANLARI KAYDED?LD?[10-12]
 hueMask = (H >= 70) & (H <= 125);
-satMask = (S >= 60) & (S <= 120);
-valMask = (V >= 70) & (V <= 120);
+#satMask = (S >= 60) & (S <= 120);
+#valMask = (V >= 70) & (V <= 120);
 #HUE,SATURATION VE VALUE DEGERLERI ICIN MINUMUM VE MAKSIMUM ESIK DEGERLERI
 #TANIMLANDI [14-16]
-maske = uint8(hueMask & satMask & valMask);
+maske = uint8(hueMask );
 maskAna = FG.*maske;
 imwrite(maskAna,"C:/Users/ismail/Desktop/d/maskAna.jpg");
 #BELIRLENEN ESIK DEGERLERINE GORE MASKE OLUSTURULDU VE ON YUZ FOTORAGRAFI
@@ -44,15 +44,15 @@ imwrite(mask1,"C:/Users/ismail/Desktop/d/mask1.jpg");
 #VALUE KATMANI ALINMIS RESIM FOR DONGUSUNE SOKULARAK VALUE DEGERI 50 YE ES?T 
 #VEYA 50 N?N ALTINDA ?SE ?K? TANE B?RB?R?N?N TAM TERS? OLAN MASKELEME 
 #GORUNTULERINI CIKTI OLARAK VERIR[31-43]
-FGO(:,:,1)=mask1.*FG(:,:,1);
-FGO(:,:,2)=mask1.*FG(:,:,2);
-FGO(:,:,3)=mask1.*FG(:,:,3);
+FGO(:,:,1)=mask2.*FG(:,:,1);
+FGO(:,:,2)=mask2.*FG(:,:,2);
+FGO(:,:,3)=mask2.*FG(:,:,3);
 imwrite(FGO,"C:/Users/ismail/Desktop/d/FGO.jpg");
 #ON YUZ FOTOGRAFI ILE ON YUZ MASKELEME MASKESINI CARPARAK ON YUZ ÇIKTI
 #FOTOGRAFINI VERIR[47-50]
-BGO(:,:,1)=mask2.*BG(:,:,1);
-BGO(:,:,2)=mask2.*BG(:,:,2);
-BGO(:,:,3)=mask2.*BG(:,:,3);
+BGO(:,:,1)=mask1.*BG(:,:,1);
+BGO(:,:,2)=mask1.*BG(:,:,2);
+BGO(:,:,3)=mask1.*BG(:,:,3);
 imwrite(BGO,"C:/Users/ismail/Desktop/d/BGO.jpg");
 #ARKA YUZ FOTOGRAFI ILE ARKA YUZ MASKELEME MASKESINI CARPARAK ARKA YUZ ÇIKTI
 #FOTOGRAFINI VERIR[53-56]
